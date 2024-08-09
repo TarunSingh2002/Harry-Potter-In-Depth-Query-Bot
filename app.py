f6d5c70d963eb9a74c04a454de0acff2ac0ffeb4
Tarun Singh <133011792+TarunSingh2002@users.noreply.github.com>
2024-08-09 10:23:16 +0530

    2.1 bearly working model

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from flask import Flask, render_template
 from langchain_openai import OpenAIEmbeddings
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
+from flask import Flask, render_template, request, jsonify
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 app = Flask(__name__)
@@ -15,6 +15,9 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+# Global variable for retrieval_chain
+retrieval_chain = None
 
 def load_vector_db(persist_directory: Path) -> Chroma:
     db = Chroma(persist_directory=str(persist_directory), embedding_function=OpenAIEmbeddings())
@@ -30,6 +33,7 @@ def create_prompt():
     return prompt
 
 def main():
+    global retrieval_chain
     current_path = Path(__file__)
     root_path = current_path.parent
     vector_db_data_path = root_path / 'data' / 'vector_db'
@@ -43,13 +47,17 @@ def main():
     document_chain=create_stuff_documents_chain(llm, prompt)
 
     retrieval_chain = create_retrieval_chain(retriever,document_chain)
-    
-    response = retrieval_chain.invoke({"input":'harry potter, Hermione Granger and ron mother names ?'})
-    print(response['answer'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        user_input = request.json.get('input_text')
+        response = retrieval_chain.invoke({"input": user_input})
+        answer = response['answer']
+        return jsonify({'answer': answer})
+    
     return render_template('index.html')
 
 if __name__ == "__main__":
+    main()
     app.run(debug=True)
